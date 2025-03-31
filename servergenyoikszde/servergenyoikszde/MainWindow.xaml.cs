48e54dca4e1eee4e5081b4c196f1ba2fe242a33c
HajduBalazs101 <hajdub@kkszki.hu>
2025-03-31 08:40:46 +0200

    asd

--- servergenyoikszde/servergenyoikszde/MainWindow.xaml.cs
+++ servergenyoikszde/servergenyoikszde/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,15 +13,28 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ZstdSharp.Unsafe;
+
 
 namespace servergenyoikszde
 {
 
     public partial class MainWindow : Window
     {
+        //kapcsolati string
+        MySqlConnection kapcs = new MySqlConnection("server=localhost;database=asztali_11a;user id=root;password=''");
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+               kapcs.Open();
+            MySqlCommand parancs = kapcs.CreateCommand();
+            var sql = $"SELECT * FROM user WHERE nev = '{txtNev.Text}' AND jelszo = '{txtJelszo.Text}' ;";
+        }
+
+        
     }
 }
